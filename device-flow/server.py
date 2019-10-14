81f7c41a5c32b104ffc6cc1767d01843188b1e14
Ben Dechrai <ben@dechrai.com>
2019-10-14 05:32:45 +0100

    Updated servo positions and make output nicer

--- device-flow/server.py
+++ device-flow/server.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
-import requests
+import string
+import json
+import httplib
 import time
+import datetime
 import jwt
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+import emoji
+
+ServoPin = 32
+ServoLockPosition = 5
+ServoUnlockPosition = 10.5
 
 auth_client_id = "ZLN1lbika9cy8BnAyJSmE354yPGZpkK8"
 auth_audience='opensesame://door1/'
@@ -19,182 +27,169 @@ kQIDAQAB
 -----END PUBLIC KEY-----
 '''
 
-code_request = requests.post('https://bendechrai.auth0.com/oauth/device/code', data={'client_id': auth_client_id, 'scope': 'openid profile', 'audience': auth_audience})
-deviceConfig = code_request.json()
+GPIO.setmode(GPIO.BOARD)               # Set the board mode to numbers pins by physical location
+GPIO.setup(ServoPin, GPIO.OUT)         # Set Servo Pin mode as output
+ServoPosition = GPIO.PWM(ServoPin, 50) # Set PWM to 50Hz
+ServoPosition.start(ServoLockPosition) # Default servo to locked position
+GPIO.cleanup()
+DoorLocked = True
 
-print("------------------------------")
-print("Log in at " + deviceConfig["verification_uri_complete"]);
-print("------------------------------")
+def loop():
+
+    conn = httplib.HTTPSConnection('bendechrai.auth0.com');
+    conn.request('POST', '/oauth/device/code', json.dumps({'client_id': auth_client_id, 'scope': 'openid profile', 'audience': auth_audience}), {"content-type": "application/json"})
+    code_request = conn.getresponse()
+    deviceConfig = json.loads(code_request.read())
+
+    printDeviceConfig(deviceConfig)
+
+    try:
+        tokens = getToken(deviceConfig);
+        id_token = jwt.decode(tokens["id_token"], auth_pubkey, audience=auth_client_id)
+        access_token = jwt.decode(tokens["access_token"], auth_pubkey, audience=auth_audience)
+
+        printTokenPayload(id_token)
+        printTokenPayload(access_token)
+
+        # CALC SECONDS TO TOKEN EXPIRY
+        timeNow=datetime.datetime.now().replace(microsecond=0)
+        timeExp=datetime.datetime.fromtimestamp(access_token['exp'])
+        timeRemaining=timeExp-timeNow
+
+        # PRINT SUCCESS MESSAGE
+        print('Door 1 can be unlocked until ' + timeExp.strftime('%a %d %B at %H:%M:%S') + ' (' + str(timeRemaining.total_seconds()) + ' seconds remaining)')
+
+        unlockDoor()
+
+
+    # IF TOKEN EXPIRED
+    except jwt.exceptions.ExpiredSignatureError:
+
+        # DECODE TOKEN WITHOUT VERIFICATION
+        access_token = jwt.decode(tokens["access_token"], verify=False)
+        printTokenPayload(access_token)
+
+        # HOW LONG AGO DID IT EXPIRE?
+        timeNow=datetime.datetime.now().replace(microsecond=0)
+        timeExp=datetime.datetime.fromtimestamp(access_token['exp'])
+        timeAgo=timeNow-timeExp
+
+        # PRINT ERROR MESSAGE
+        print('Access denied - token expired at ' + timeExp.strftime('%a %d %B at %H:%M:%S') + ' (' + str(timeAgo.total_seconds()) + ' seconds ago)')
+
+        lockDoor()
+        pass
+
+    except jwt.exceptions.DecodeError:
+        print('Card content did not look like a JWT')
+        pass
+
+    except MemoryError:
+        pass
 
 def getToken(deviceConfig):
-  time.sleep(deviceConfig["interval"])
-  token_request = requests.post('https://bendechrai.auth0.com/oauth/token', data={'grant_type': 'urn:ietf:params:oauth:grant-type:device_code', 'device_code': deviceConfig["device_code"], 'client_id': auth_client_id})
-  token = token_request.json()
+    time.sleep(deviceConfig["interval"])
 
-  if 'error' in token:
+    print(emoji.emojize('\n:eyes:'))
 
-    if token["error"] == 'authorization_pending':
-      # User has yet to authorize device code - recurse and try again
-      return getToken(deviceConfig)
+    conn = httplib.HTTPSConnection('bendechrai.auth0.com');
+    conn.request('POST', '/oauth/token', json.dumps({'grant_type': 'urn:ietf:params:oauth:grant-type:device_code', 'device_code': deviceConfig["device_code"], 'client_id': auth_client_id}), {"content-type": "application/json"})
+    token_request = conn.getresponse()
+    tokens = json.loads(token_request.read())
 
-    if token["error"] == 'expired_token':
-      # The device code has expired - return empty json object
-      return "{}".json()
+    if 'error' in tokens:
 
-  if 'id_token' in token and 'access_token' in token:
-    # Return token
-    return token
+        if tokens["error"] == 'authorization_pending':
+            # User has yet to authorize device code - recurse and try again
+            print(emoji.emojize(':thumbs_down:'))
+            return getToken(deviceConfig)
 
-token = getToken(deviceConfig);
-id_token = jwt.decode(token["id_token"], auth_pubkey, audience=auth_client_id)
-access_token = jwt.decode(token["access_token"], auth_pubkey, audience=auth_audience)
+        if tokens["error"] == 'expired_token':
+            # The device code has expired - return empty json object
+            print(emoji.emojize(':thumbs_down:'))
+            return "{}".json()
 
-print(id_token)
-print(access_token)
+    if 'id_token' in tokens and 'access_token' in tokens:
+        # Return tokens
+        print(emoji.emojize(':thumbs_up:'))
+        return tokens
+
+def printDeviceConfig(deviceConfig):
+        print('+-----------------------------------------------------------------------------------------------------+')
+        print('| DEVICE CONFIG                                                                                       |')
+        print(string.ljust('|               DEVICE CODE: ' + deviceConfig['device_code'],               102, ' ') + '|')
+        print(string.ljust('|                  INTERVAL: ' + str(deviceConfig['interval']),             102, ' ') + '|')
+        print(string.ljust('|                EXPIRES IN: ' + str(deviceConfig['expires_in']),           102, ' ') + '|')
+        print(string.ljust('|          VERIFICATION URI: ' + deviceConfig['verification_uri'],          102, ' ') + '|')
+        print(string.ljust('| VERIFICATION URI COMPLETE: ' + deviceConfig['verification_uri_complete'], 102, ' ') + '|')
+        print(string.ljust('|                 USER CODE: ' + deviceConfig['user_code'],                 102, ' ') + '|')
+        print('+-----------------------------------------------------------------------------------------------------+')
+
+def printTokenPayload(token_payload):
+        # SHOW TOKEN PAYLOAD
+        print('+---------------------------------------------------------------------------------------------------+')
+        print('| TOKEN:                                                                                            |')
+        if( 'sub' in token_payload):
+            print(string.ljust('|      SUB: ' + token_payload['sub'],                     100, ' ') +                 '|')
+        if( 'name' in token_payload):
+            print(string.ljust('|     NAME: ' + token_payload['name'],                    100, ' ') +                 '|')
+        if( 'nickname' in token_payload):
+            print(string.ljust('| NICKNAME: ' + token_payload['nickname'],                100, ' ') +                 '|')
+        if( 'iat' in token_payload):
+            timeIat=datetime.datetime.fromtimestamp(token_payload['iat'])
+            print(string.ljust('|      IAT: ' + timeIat.strftime('%a %d %B at %H:%M:%S'), 100, ' ') +                 '|')
+        if( 'exp' in token_payload):
+            timeExp=datetime.datetime.fromtimestamp(token_payload['exp'])
+            print(string.ljust('|      EXP: ' + timeExp.strftime('%a %d %B at %H:%M:%S'), 100, ' ') +                 '|')
+        if( 'aud' in token_payload):
+            print(string.ljust('|      AUD: ' + str(token_payload['aud']),                100, ' ') +                 '|')
+        if( 'iss' in token_payload):
+            print(string.ljust('|      ISS: ' + token_payload['iss'],                     100, ' ') +                 '|')
+        if( 'azp' in token_payload):
+            print(string.ljust('|      AZP: ' + token_payload['azp'],                     100, ' ') +                 '|')
+        if( 'scope' in token_payload):
+            print(string.ljust('|    SCOPE: ' + token_payload['scope'],                   100, ' ') +                 '|')
+        print('+---------------------------------------------------------------------------------------------------+')
+
+def lockDoor():
+    global DoorLocked
+    if(not DoorLocked):
+        DoorLocked = True
+        print(emoji.emojize(":lock: :lock: :lock:  LOCK DOOR  :lock: :lock: :lock:", use_aliases=True))
+        GPIO.setmode(GPIO.BOARD)               # Set the board mode to numbers pins by physical location
+        GPIO.setup(ServoPin, GPIO.OUT)         # Set Servo Pin mode as output
+        ServoPosition.start(ServoLockPosition)
+        time.sleep(0.1)
+        GPIO.cleanup()
+
+def unlockDoor():
+    global DoorLocked
+    if(DoorLocked):
+        DoorLocked = False
+        print(emoji.emojize(":unlock: :unlock: :unlock: UNLOCK DOOR :unlock: :unlock: :unlock:", use_aliases=True))
+        GPIO.setmode(GPIO.BOARD)               # Set the board mode to numbers pins by physical location
+        GPIO.setup(ServoPin, GPIO.OUT)         # Set Servo Pin mode as output
+        ServoPosition.start(ServoUnlockPosition)
+        time.sleep(0.1)
+        GPIO.cleanup()
+
+        # LOCK DOOR AFTER 5 SECONDS
+        time.sleep(1)
+        print('Door 1 locking in 4 seconds')
+        time.sleep(1)
+        print('Door 1 locking in 3 seconds')
+        time.sleep(1)
+        print('Door 1 locking in 2 seconds')
+        time.sleep(1)
+        print('Door 1 locking in 1 seconds')
+        time.sleep(1)
+        lockDoor()
+        time.sleep(1)
 
 
-# ServoPin = 32
-# ServoUnlockPosition = 5
-# ServoLockPosition = 10
-# 
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(ServoPin, GPIO.OUT)
-# ServoPosition = GPIO.PWM(ServoPin, 50)
-# ServoPosition.start(ServoLockPosition)
 
-
-#             print('Door 1 can be unlocked until ' + timeExp.strftime('%a %d %B at %H:%M:%S') + ' (' + str(timeRemaining.total_seconds()) + ' seconds remaining)')
-#             if(DoorLocked):
-#                 DoorLocked = False
-#                 print("Unlock Door")
-#                 ServoPosition.ChangeDutyCycle(ServoUnlockPosition)
-#                 print("Wait 5 seconds (let someone open the door)")
-#                 time.sleep(5)
-# 
-#         except jwt.exceptions.ExpiredSignatureError:
-#             print('Access denied')
-#             if(not DoorLocked):
-#                 DoorLocked = True
-#                 print("Lock Door")
-#                 ServoPosition.ChangeDutyCycle(ServoLockPosition)
-#             pass
-# 
-#         except nxppy.SelectError:
-#             if(not DoorLocked):
-#                 DoorLocked = True
-#                 print("Lock Door")
-#                 ServoPosition.ChangeDutyCycle(ServoLockPosition)
-#             pass
-# 
-#         except jwt.exceptions.DecodeError:
-#             print('Card content did not look like a JWT')
-#             pass
-# 
-#         except MemoryError:
-#             pass
-# 
-#         print('Waiting')
-#         time.sleep(0.5)
-# 
-# 
-# def destroy():
-#     GPIO.cleanup()
-# 
-# 
-# 
-# def getNFCPayload(data):
-# 
-#     # Remove the NFC tag header and length from the data - store them even though we ignore them
-#     printIfDebug("# Remove the NFC tag header and length from the data - store them even though we ignore them")
-#     printIfDebug(list(data))
-#     header = ord(data[0])
-#     length = ord(data[1])
-#     printIfDebug("Header: " + str(header))
-#     printIfDebug("Length: " + str(length))
-# 
-#     # NDEF Message is the data from char 3 onwards
-#     printIfDebug("# NDEF Message is the data from char 3 onwards")
-#     ndef_message = data[2:]
-#     printIfDebug("NDEF Message")
-#     printIfDebug(list(ndef_message))
-# 
-#     # Remove the NDEF header from the message
-#     printIfDebug("# Remove the NDEF header from the message")
-#     ndef_header = ord(ndef_message[0])
-#     ndef_message = ndef_message[1:]
-#     printIfDebug("NDEF Header: " + str(ndef_header))
-#     printIfDebug("NDEF Message")
-#     printIfDebug(list(ndef_message))
-# 
-#     # Remove the NDEF Type Length from the message
-#     printIfDebug("# Remove the NDEF Type Length from the message")
-#     #ndef_type_length = ord(ndef_message[0])
-#     ndef_type_length = 1
-#     ndef_message = ndef_message[1:]
-#     printIfDebug("NDEF Type Length: " + str(ndef_type_length))
-#     printIfDebug("NDEF Message")
-#     printIfDebug(list(ndef_message))
-# 
-# 
-#     # Remove the Payload Length from the message
-#     printIfDebug("# Remove the Payload Length from the message")
-#     # NDEF Header bit 4 is the SR flag. 1 = PAYLOAD LENGTH field is 1 byte. 0 = PAYLOAD Length field is 4 bytes.
-#     if(ndef_header & 16 == 0):
-#         ndef_message = ndef_message[2:] # For some reason, non SR lengths have two extra bytes before the length. @TODO why?
-#         payload_length = 0
-#         payload_length += ord(ndef_message[0]) * 256 * 256 * 256
-#         payload_length += ord(ndef_message[1]) * 256 * 256
-#         payload_length += ord(ndef_message[2]) * 256
-#         payload_length += ord(ndef_message[3])
-#         ndef_message = ndef_message[4:]
-#     else:
-#         payload_length = ord(ndef_message[0])
-#         ndef_message = ndef_message[1:]
-#     printIfDebug("Payload Length: " + str(payload_length))
-#     printIfDebug("NDEF Message")
-#     printIfDebug(list(ndef_message))
-# 
-#     # Remove ID Length from the message, if IL flag (bit 3) is set
-#     printIfDebug("# Remove ID Length from the message, if IL flag (bit 3) is set")
-#     if(ndef_header & 8 == 8):
-#         ndef_id_length = ord(ndef_message[0])
-#         ndef_message = ndef_message[1:]
-#     printIfDebug("NDEF Message")
-#     printIfDebug(list(ndef_message))
-# 
-#     # Remove Record Type from the message
-#     printIfDebug("# Remove Record Type from the message")
-#     ndef_type = ndef_message[:ndef_type_length]
-#     ndef_message = ndef_message[ndef_type_length:]
-#     printIfDebug("NDEF Type: " + ndef_type)
-#     printIfDebug("NDEF Message")
-#     printIfDebug(list(ndef_message))
-# 
-#     # Remove Payload from the message
-#     payload = ndef_message[:payload_length]
-#     ndef_message = ndef_message[payload_length:]
-#     printIfDebug("Payload: " + payload)
-#     printIfDebug("NDEF Message")
-#     printIfDebug(list(ndef_message))
-# 
-#     # Extract language and token from payload
-#     payload_encoding = ord(payload[0:1])
-#     payload_lang = payload[1:3]
-#     payload = payload[3:payload_length]
-#     printIfDebug("Payload Encoding: " + str(payload_encoding))
-#     printIfDebug("Payload Language: " + payload_lang)
-#     printIfDebug("Payload: " + payload)
-# 
-#     return payload
-# 
-# 
-# 
-# def printIfDebug(output):
-#     if(debug):
-#         print(output)
-# 
-# if __name__ == '__main__':     # Program start from here
-#     try:
-#         loop()
-#     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the destroy() will be  executed.
-#         destroy()
+if __name__ == '__main__':     # Program start from here
+    try:
+        loop()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the destroy() will be  executed.
+        GPIO.cleanup()
