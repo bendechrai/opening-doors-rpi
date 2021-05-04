f5164c9f819dc2fc8984a1036d0f088bcd9dc828
Ben Dechrai <ben@dechrai.com>
2021-05-04 14:16:25 +0100

    Moved keys to excluded file

---
+++ door-bell-sms/config-example.py
@@ -0,0 +1,19 @@
+auth_client_id = "vJF3okNM6n1ryyRU7jH4uomKCH4fMw04"
+auth_domain = "opening-doors.auth0.com"
+auth_audience = "opensesame://door1/"
+auth_pubkey = '''-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvupG3F3hKRzg8cxvF9Nr
+QwPyCaJ0Vzi+jj/c3F3hCdhMEjZ8hFFq3VOK4R47B97bi9cft5J9gqVZ5QYjppMq
+idWn10HvIvxSfXvaGdMCrAyhJaCSYOVFk2g7ZrxEwl1pv4l8rrTqs1cwWo+6m1kK
+mxs8tF7PvyECVWx0jUoJOskDj/2LK2x4KfZdJztlvtB8J8yd8L/Pa/HQNSlIyB2i
+7njosJxiW5cIMaB4GUIORmX4apnThuMH0jtSwBJyo/CNUGcGyEhAQww/uwBnFhtV
+pPpKN1cyF/4iKV+zhDmnKkyHASoqzN+2uOFoDpL9CoxC8mbslWy/85zEQSR5Nqlb
+5QIDAQAB
+-----END PUBLIC KEY-----'''
+
+twilio_sid = "???"
+twilio_token = "???"
+
+jsonbin_domain = "api.jsonbin.io"
+jsonbin_request = "https://api.jsonbin.io/v3/b/???/latest"
+jsonbin_master_key = "???"
--- door-bell-sms/server.py
+++ door-bell-sms/server.py
@@ -10,25 +10,7 @@ import jwt
 import RPi.GPIO as GPIO
 import emoji
 from twilio.rest import Client
-
-auth_client_id = "vJF3okNM6n1ryyRU7jH4uomKCH4fMw04"
-auth_domain='opening-doors.auth0.com'
-auth_audience='opensesame://door1/'
-auth_pubkey='''-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvupG3F3hKRzg8cxvF9Nr
-QwPyCaJ0Vzi+jj/c3F3hCdhMEjZ8hFFq3VOK4R47B97bi9cft5J9gqVZ5QYjppMq
-idWn10HvIvxSfXvaGdMCrAyhJaCSYOVFk2g7ZrxEwl1pv4l8rrTqs1cwWo+6m1kK
-mxs8tF7PvyECVWx0jUoJOskDj/2LK2x4KfZdJztlvtB8J8yd8L/Pa/HQNSlIyB2i
-7njosJxiW5cIMaB4GUIORmX4apnThuMH0jtSwBJyo/CNUGcGyEhAQww/uwBnFhtV
-pPpKN1cyF/4iKV+zhDmnKkyHASoqzN+2uOFoDpL9CoxC8mbslWy/85zEQSR5Nqlb
-5QIDAQAB
------END PUBLIC KEY-----'''
-
-twilio_sid='ACc3e25e6d1a9a5b1303e2d7d00a28533f'
-twilio_token='315f882052f0d967ecb1bf389f820e0d'
-jsonbin_domain='api.jsonbin.io'
-jsonbin_request='https://api.jsonbin.io/v3/b/608f8f7ad64cd16802a79ff7/latest'
-jsonbin_master_key="$2b$10$010U3SzJzSr9TtkP3YCl/Okk4CQJ/vsOzIxV0hpVJp0eCUwtlPT7m"
+from config import *
 
 ServoPin = 32
 GPIO.setmode(GPIO.BOARD)
@@ -78,9 +60,6 @@ def handleDoorBell():
     jsonbin_conn.request('GET', jsonbin_request, '', { "X-Master-key": jsonbin_master_key })
     jsonbin_response = jsonbin_conn.getresponse()
     jsonbin_data = json.loads(jsonbin_response.read())
-    # {'record': {'number': '+61415127120', 'name': 'joh'}, 'metadata': {'id': '608f8f7ad64cd16802a79ff7', 'private': True, 'createdAt': '2021-05-03T06:14:16.956Z'}}
-
-    print(jsonbin_data)
 
     twilio_client = Client(twilio_sid, twilio_token)
 
